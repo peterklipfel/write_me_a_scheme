e75d4f7df20282da31dc70d7f1a97a5a97d7c6f5
Peter Klipfel <peter@klipfel.me>
2014-05-30 23:24:22 -0600

    function for parsing

--- parser.hs
+++ parser.hs
@@ -6,3 +6,8 @@ symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 main :: IO()
 main = do
   putStrLn ("I should parse something")
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "Could not find matching expression: " ++ show err
+  Right val -> "Valid Value"
