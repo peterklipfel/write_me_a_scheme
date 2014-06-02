459d771bfa89f0faca37691a34e673db0b8a8156
Peter Klipfel <peter@klipfel.me>
2014-06-01 22:50:08 -0600

    parsing simple lisp expressions

--- parser.hs
+++ parser.hs
@@ -1,13 +1,51 @@
+import Control.Monad
+import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+             | List[LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many $ letter <|> digit <|> symbol
+  let atom = [first] ++ rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    otherwise -> Atom atom 
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many $ noneOf "\""
+  char '"'
+  return $ String x
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "Could not find matching expression: " ++ show err
+  Right val -> "Valid Value"
+
+spaces :: Parser()
+spaces = skipMany1 space
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
+
 main :: IO()
 main = do
-  putStrLn ("I should parse something")
+  args <- getArgs
+  putStrLn . readExpr $ args !! 0
 
-readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-  Left err -> "Could not find matching expression: " ++ show err
-  Right val -> "Valid Value"
