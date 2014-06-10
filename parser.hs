a95fc6bff5d1e86f737aa068f10e8288191fc458
Peter Klipfel <peter@klipfel.me>
2014-06-09 18:52:08 -0600

    support for lists/

--- parser.hs
+++ parser.hs
@@ -46,6 +46,37 @@ strBoolBinop = boolBinOp unpackStr
 
 car :: [LispVal] -> ThrowsError LispVal
 car [List (x:xs)] = return x
+car [DottedList (x:xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x:xs)] = return $ List xs
+cdr [DottedList (_:xs) x] = return $ DottedList xs x
+cdr [DottedList [xs] x] = return x
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List[]] = return $ List [x1]
+cons [x, List xs] = return $ List $ [x] ++ xs
+cons [x, DottedList xs xlast] = return $ DottedList ([x] ++ xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgs = throwError $ NumArgs 2 badArgs
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) &&
+                                                    (and $ map eqvPair $ zip arg1 arg2)
+  where eqvPair (x1, x2) = case eqv [x1, x2] of 
+                            Left err -> False
+                            Right (Bool val) -> val
+eqv [_, _] = return $ Bool False
+eqv badArgs = throwError $ NumArgs 2 badArgs
 
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
@@ -133,7 +164,11 @@ primitives = [("+", numericBinop (+)),
               ("string<=?",strBoolBinop (<=)),
               ("string<?",strBoolBinop (<)),
               ("string>?",strBoolBinop (>)),
-              ("string>=?",strBoolBinop (>=))]
+              ("string>=?",strBoolBinop (>=)),
+              ("cdr", cdr),
+              ("car", car),
+              ("eq?", eqv),
+              ("eqv?", eqv)]
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
